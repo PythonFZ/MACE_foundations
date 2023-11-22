b473b2b4c61e3f6818758ff214e6d2ef4e8e50c2
PythonFZ <fabian.zills@web.de>
2023-11-22 13:03:46 +0100

    BMIM_Cl MD

---
+++ main.py
@@ -0,0 +1,71 @@
+import ipsuite as ips
+import zntrack
+from src import LoadModel
+
+project = ips.Project(automatic_node_names=True)
+
+thermostat = ips.calculators.LangevinThermostat(
+    temperature=298.15, friction=0.01, time_step=0.5
+)
+
+with project:
+    model = LoadModel(model_path="2023-08-14-mace-universal.model")
+
+ramp_density = ips.calculators.RescaleBoxModifier(
+    density=1086
+)
+
+ramp_temperature = ips.calculators.TemperatureRampModifier(
+    start_temperature=250, temperature=500,
+)
+
+thermostat = ips.calculators.LangevinThermostat(
+    temperature=298.15, friction=0.01, time_step=0.5
+)
+
+with project.group("BMIM_Cl"):
+    cation = ips.configuration_generation.SmilesToAtoms("CCCCN1C=C[N+](=C1)C")
+    anion = ips.configuration_generation.SmilesToAtoms("[Cl-]")
+
+    single_structure = ips.configuration_generation.Packmol(
+            data=[cation.atoms, anion.atoms],
+            count=[1, 1],
+            density=1000,
+            pbc=False
+    )
+
+    structure = ips.configuration_generation.Packmol(
+        data=[single_structure.atoms],
+        count=[10],
+        density=700,
+    )
+    
+    geo_opt = ips.calculators.ASEGeoOpt(
+        model=model,
+        data=structure.atoms,
+        data_id=-1,
+        optimizer="FIRE",
+        run_kwargs={"fmax": 0.5},
+    )
+
+    density_md = ips.calculators.ASEMD(
+        data=geo_opt.atoms,
+        data_id=-1,
+        model=model,
+        modifier=[ramp_density],
+        thermostat=thermostat,
+        steps=1000,
+        sampling_rate=10,
+    )
+
+    md = ips.calculators.ASEMD(
+        data=geo_opt.atoms,
+        data_id=-1,
+        model=model,
+        modifier=[ramp_temperature],
+        thermostat=thermostat,
+        steps=500_000,
+        sampling_rate=100,
+    )
+
+project.build()
---
+++ src/__init__.py
@@ -0,0 +1,38 @@
+import zntrack
+import torch 
+import functools
+import ase
+from mace.calculators.foundations_models import mace_mp
+from mace.calculators import MACECalculator
+from ase.calculators.singlepoint import SinglePointCalculator
+import tqdm
+
+class LoadModel(zntrack.Node):
+    model_path: str = zntrack.deps_path()
+
+    def run(self) -> None:
+        pass
+    
+    @functools.lru_cache()
+    def get_model(self, **kwargs) -> torch.nn.Module:
+        with self.state.fs.open(self.model_path, 'rb') as f:
+            if kwargs.get("device", None) is None:
+                return torch.load(f)
+            else:
+                model = torch.load(f, map_location=kwargs["device"])
+                model.to(kwargs["device"])
+                return model
+    
+    def get_calculator(self, device=None, default_dtype="float32", **kwargs) -> MACECalculator:
+        return mace_mp(model=self.model_path, dispersion=False)
+
+    def predict(self, atoms_list: list[ase.Atoms]) -> list[ase.Atoms]:
+        calc = self.get_calculator()
+        for atoms in tqdm.tqdm(atoms_list):
+            atoms.set_calculator(calc)
+            energy = atoms.get_potential_energy()
+            forces = atoms.get_forces()
+            atoms.set_calculator(
+                SinglePointCalculator(atoms, energy=energy, forces=forces)
+            )
+        return atoms_list
