3cda973c15e6acaa44de50ef488ac0f115c95965
PythonFZ <fabian.zills@web.de>
2023-11-22 13:10:39 +0100

    DFT params

--- main.py
+++ main.py
@@ -72,6 +72,12 @@ with project.group("BMIM_Cl"):
         data=md.atoms,
         n_configurations=50,
     )
-    
+
+    cp2k = ips.calculators.CP2KSinglePoint(
+        data=selection.atoms,
+        cp2k_params="config/cp2k.yaml",
+        cp2k_files=["GTH_BASIS_SETS", "GTH_POTENTIALS", "dftd3.dat"],
+    )
+
 
 project.build()
